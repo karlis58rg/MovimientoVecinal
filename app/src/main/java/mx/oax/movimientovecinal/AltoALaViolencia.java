47fbfebfc75e28049b64db821da5ed73f3d70a55
karlis58rg@gmail.com <karlis58rg@gmail.com>
2020-12-09 17:16:22 -0600

    AlertaAmber

---
+++ app/src/main/java/mx/oax/movimientovecinal/AltoALaViolencia.java
@@ -0,0 +1,14 @@
+package mx.oax.movimientovecinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AltoALaViolencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alto_ala_violencia);
+    }
+}
---
+++ app/src/main/java/mx/oax/movimientovecinal/VigilanciaVecinal.java
@@ -0,0 +1,14 @@
+package mx.oax.movimientovecinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VigilanciaVecinal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vigilancia_vecinal);
+    }
+}
